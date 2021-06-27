2f14f53e183dfd83df7b08b1cdc94e6ece8e0c98
yassine khiari <65060923+yannix2@users.noreply.github.com>
2021-06-27 02:17:49 +0100

    Add files via upload

---
+++ challenge2.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+
+int main() {
+    int a,b,i;
+    printf("donner le num ' \n");
+    scanf ("%d",&a);
+
+    printf("donner le long \n");
+    scanf ("%i",&b);
+     for (i=1;i<=b;i++)
+    { int c=a*i;
+     printf ("%i \t",c);
+    }
+   
+
+
+
+    return 0;
+}
